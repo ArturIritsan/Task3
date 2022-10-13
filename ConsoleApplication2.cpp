97898d47b5569ba158a81da35c12701ebd0b6d47
ArturIritsan <115408365+ArturIritsan@users.noreply.github.com>
2022-10-13 21:21:01 +0300

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c,D;
+    cin >> a, b, c;
+    D = b * b - 4 * a * c;
+    if (D > 0); {
+        cout << "0";
+    }
+    if (D < 0); {
+        cout << "2";
+    }
+
+
+}
+    
+
